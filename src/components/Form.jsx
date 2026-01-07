4b290f245885f1ba220a1ef4b3023da811bd8f4d
sk9o9 <sumitkhadka098@gmail.com>
2026-01-07 07:16:41 +0545

    first name validation done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function Form() {
+  let {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  let formSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="grid place-content-center mt-6">
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit(formSubmit)} className="flex flex-col gap-4">
         {/* firstname */}
         <div className="flex items-center gap-3">
           <label className="whitespace-nowrap">Enter your first name</label>
           <input
             className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
             placeholder="Enter first name"
+            {...register("fname", { required: true })}
           />
+          <span className="w-40 text-sm whitespace-nowrap">
+            {errors.fname && (
+              <span className="text-red-500">* This field is required</span>
+            )}
+          </span>
         </div>
 
         {/* lastname */}
@@ -71,11 +88,8 @@ function Form() {
 
         {/* submit button */}
         <div className="flex justify-end mt-4">
-          <button
-            type="button"
-            class="text-white bg-linear-to-r from-purple-500 to-pink-500 hover:bg-linear-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5"
-          >
-            Purple to Pink
+          <button class="text-white bg-linear-to-r from-purple-500 to-pink-500 hover:bg-linear-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl">
+            Submit
           </button>
         </div>
       </form>
